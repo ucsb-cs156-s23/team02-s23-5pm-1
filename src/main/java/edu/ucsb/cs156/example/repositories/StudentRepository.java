3a873eb49f53a33fda144a298cbcf21cf99bbc20
Elijah <ElijahFrankle626@gmail.com>
2023-05-09 14:17:56 -0700

    added Students Table

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/StudentRepository.java
@@ -0,0 +1,12 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, String> {
+
+}
